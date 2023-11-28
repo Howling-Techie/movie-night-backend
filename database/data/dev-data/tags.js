7a66a6a10150c904f6692575ca7d339a68e04b39
Howling Techie <howling.techie@gmail.com>
2023-11-28 17:11:25 +0000

    Fixed formatting for dev data

--- database/data/dev-data/tags.js
+++ database/data/dev-data/tags.js
@@ -1,1 +1,6 @@
-module.exports = [];
+module.exports = [
+    {tag_id: 1, server_id: "746772975621242970", name: "Awful", description: "Truly the best of the worst", icon: "🗑️"},
+    {tag_id: 2, server_id: "746772975621242970", name: "Spooky", description: "Don't have nightmares", icon: "👻"},
+    {tag_id: 3, server_id: "746772975621242970", name: "Awesome", description: "Good movies only!", icon: "🌟"},
+    {tag_id: 4, server_id: "746772975621242970", name: "Franchise", description: "You're gonna finish it!", icon: "🔢"}
+];
